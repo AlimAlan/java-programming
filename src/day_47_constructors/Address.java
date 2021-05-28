6776f8a7b9998da2d9036f679c048ee3c0015f7d
MeerimAlimbekova <alimbekova25m@gmail.com>
2021-05-27 21:39:26 -0400

    added constructors to Address.java and AddressList

--- src/day_47_constructors/Address.java
+++ src/day_47_constructors/Address.java
@@ -6,6 +6,23 @@ public class Address {
     private String state;
     private String zipCode;
     private String country="USA";
+    //constructor -automatically called
+    public Address(){
+        System.out.println("Address constructor");
+        street="123 unknown street";
+        city="Unknown";
+        state="Unknown";
+        zipCode="00000";
+    }
+    //second constructor, overloaded -> provides shortcut to initialize variables
+    //in same statement: ex: Address ad=new address("123 java st", "Boston", "MA","234235");
+    public Address(String street, String  city, String state, String zipCode){
+        this.street=street;
+        setStreet(street);
+        this.city=city;
+        this.state=state;
+       // this.
+    }
 
 
     public String toString(){
@@ -15,6 +32,7 @@ public class Address {
     public String getStreet() {
         return street;
     }
+
 
     public void setStreet(String street) {
         this.street = street;
