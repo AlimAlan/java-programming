1f8bbd0576f396f3eafa1ff9e4eaaddecec4f63f
MeerimAlimbekova <alimbekova25m@gmail.com>
2021-05-29 17:07:47 -0400

    static method demo using Calculator

--- src/day_48_constructors/static_examples/CalculatorTest.java
+++ src/day_48_constructors/static_examples/CalculatorTest.java
@@ -4,9 +4,9 @@ public class CalculatorTest {
     //static method
     public static void main(String[] args) {
         Calculator.add(5,3);  //add is static method, can be called using clasname.staticMethodName
+//1
 
-
-        //calculator.multiply (2,4) error multuply is not a static, need object to call it
+        //calculator.multiply (2,4) error multiply is not a static, need object to call it
 
         //multiply is instance method, and we are creating object then calling it
         Calculator calculator=new Calculator();
